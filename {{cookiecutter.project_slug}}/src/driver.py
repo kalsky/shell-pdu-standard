4515127c6e50f4e57211de12e43fcc251251a4cd
kalsky <yanivka@gmail.com>
2016-08-03 11:13:57 -0700

    update power driver template to include the power commands

--- {{cookiecutter.project_slug}}/src/driver.py
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -1,8 +1,9 @@
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 from cloudshell.shell.core.context import InitCommandContext, ResourceCommandContext
+from cloudshell.shell.core.context_utils import context_from_args
+from cloudshell.power.pdu.power_resource_driver_interface import PowerResourceDriverInterface
 
-
-class {{cookiecutter.driver_name}} (ResourceDriverInterface):
+class {{cookiecutter.driver_name}} (ResourceDriverInterface, PowerResourceDriverInterface):
 
     def cleanup(self):
         """
@@ -17,6 +18,7 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         """
         pass
 
+    @context_from_args
     def initialize(self, context):
         """
         Initialize the driver session, this function is called everytime a new instance of the driver is created
@@ -25,6 +27,43 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         """
         pass
 
+    @context_from_args
+    def PowerOn(self, context, ports):
+        """ Powers on outlets on the managed PDU
+        :param context: context from the command which invoked PowerOn
+        :param ports: full addresses of outlets on PDU, example: ['192.168.30.128/4', '192.168.30.128/6']
+        :type ports: str
+        :returns: command result
+        :rtype: str
+        """
+        pass
+
+
+    @context_from_args
+    def PowerOff(self, context, ports):
+        """ Powers off outlets on the managed PDU
+        :param context: context from the command which invoked PowerOff
+        :param ports: full addresses of outlets on PDU, example: ['192.168.30.128/4', '192.168.30.128/6']
+        :type ports: str
+        :returns: command result
+        :rtype: str
+        """
+        pass
+
+    @context_from_args
+    def PowerCycle(self, context, ports, delay=0):
+        """ Powers off outlets, waits during delay, then powers outlets on
+        :param context: context from the command which invoked PowerCycle
+        :param ports: full addresses of outlets on PDU, example: ['192.168.30.128/4', '192.168.30.128/6']
+        :type ports: str
+        :param delay: seconds to wait after power off
+        :type delay: int
+        :returns: command result
+        :rtype: str
+        """
+        pass
+    
+    @context_from_args
     def example_function(self, context):
         """
         A simple example function
@@ -32,9 +71,10 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         """
         pass
 
+    @context_from_args
     def example_function_with_params(self, context, user_param1, user_param2):
         """
-        An example function that accepts two user parametesrs
+        An example function that accepts two user parameters
         :param ResourceCommandContext context: the context the command runs on
         :param str user_param1: A user parameter
         :param str user_param2: A user parameter
